ab89335a34321e8efda1addfd7f262e0e8472c53
fisher <tiebayu@126.com>
2018-06-23 22:35:47 +0800

    解决重复显示非吧主申请问题

--- js/tbMerge.user.js
+++ js/tbMerge.user.js
@@ -2,7 +2,7 @@
 // @name        贴吧合并功能增强
 // @namespace   https://github.com/52fisher/tbMerge
 // @author      投江的鱼
-// @version     3.0.3
+// @version     3.0.4
 // @description 适用于贴吧合并吧标准申请格式,兼容部分非标准格式内容
 // @include     http://tieba.baidu.com/p/*
 // @include     https://tieba.baidu.com/p/*
@@ -34,7 +34,7 @@
         check: function() {
             //show check tips
             $("#done-tips").hide();
-            $("#err-tips").hide();
+            $("#err-tips").empty().hide();
             $("#check-tips").show();
             // remove tb_*
             $("span[class^=tb]").remove();
@@ -133,7 +133,7 @@
             $.getJSON('/home/get/panel', {
                 un: PageData.thread.author,
                 ie: 'utf-8',
-                r: Math.random() //nocache to get newest data of manager 
+                r:Math.random()//nocache to get newest data of manager
             }, function(e) {
                 if (e.no) return;
                 var flag = false;
@@ -145,8 +145,8 @@
                         }
                     }
                 }
-                tbMerge.isDebug ? console.log('申请人吧主检测:' + (flag ? "通过" : "拒绝")) : null;
-                tbMerge.isDebug ? console.log('申请人所担任吧主:' + e.data.honor.manager.manager.forum_list.join(' ')) : null;
+                tbMerge.isDebug ? console.log('申请人吧主检测:'+ ( flag ? "通过":"拒绝" ) ):null;
+                tbMerge.isDebug ? console.log('申请人所担任吧主:'+ e.data.honor.manager.manager.forum_list.join(' ')):null;
                 tbMerge.isDebug ? console.groupEnd() : null;
                 if (!flag) {
                     tbMerge.showerr('发贴人非相关贴吧吧主', true)
