83c84f669bdd906130f6cd650c20a38f163ecd16
fisher <tiebayu@126.com>
2018-06-25 19:35:40 +0800

    解决申请人无吧主身份问题

--- js/tbMerge.meta.js
+++ js/tbMerge.meta.js
@@ -2,7 +2,7 @@
 // @name        贴吧合并功能增强
 // @namespace   https://github.com/52fisher/tbMerge
 // @author		投江的鱼
-// @version     3.0.4
+// @version     3.0.5
 // @description 适用于贴吧合并吧标准申请格式,兼容部分非标准格式内容
 // @include     http://tieba.baidu.com/p/*
 // @include     https://tieba.baidu.com/p/*
